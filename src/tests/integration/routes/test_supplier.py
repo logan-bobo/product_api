49df383ebc6b00e9342275eb7f72bb142e46538b
Logan Cox <mail@logan-cox.com>
2023-03-17 20:49:45 +0000

    adding integration test for creating a supplier

--- src/tests/integration/routes/test_supplier.py
+++ src/tests/integration/routes/test_supplier.py
@@ -15,7 +15,7 @@ class TestSupplier:
         """
         GIVEN the supplier endpoint
         WHEN an HTTP GET request is made to the supplier endpoint
-        THEN we are returned JSON that shows the supplier name
+        THEN we are returned JSON that shows all suppliers
         """
         response = client.get(f"/{API_VERSION}/suppliers")
 
@@ -46,3 +46,17 @@ class TestSupplier:
         response = client.get(f"/{API_VERSION}/suppliers/900")
 
         assert b'{"message":"supplier 900 not found"}' in response.data
+
+    def test_supplier_can_be_created(self, client):
+        """
+        GIVEN the supplier endpoint
+        WHEN an HTTP POST reqeust is made to create a new supplier based on name
+        THEN the supplier is persisted to the database and can be called via GET request based on ID
+        """
+
+        post_response = client.post(f"/{API_VERSION}/suppliers", json={"name": "fanta_industries_inc"})
+        get_response = client.get(f"/{API_VERSION}/suppliers/2")
+
+        assert b'{"message":"supplier fanta_industries_inc created"}' in post_response.data
+        assert b'{"name":"fanta_industries_inc"}' in get_response.data
+
